d668c393c12e6348e5af10b7b016fd6c15d7c142
1E10 <eabazi5484@bths.edu>
2022-09-30 12:32:38 -0400

    Changes, changes, nothing finished

--- src/Point.java
+++ src/Point.java
@@ -8,13 +8,33 @@ public class Point {
         this.y = y;
     }
 
-    public Point(int num){
+    public Point(int num) {
         x = num;
         y = num;
     }
 
-    public Point(){
+    public Point() {
         x = 0;
         y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    public String coordinate(){
+        return
     }
 }
